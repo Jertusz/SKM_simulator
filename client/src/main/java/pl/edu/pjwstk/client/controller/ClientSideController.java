5625262e67e3e44fb8407b827c8ecdac702f22e7
Jedrzej Szadejko <jendziu@gmail.com>
2020-10-24 19:25:33 +0200

    Project basically finished along with docker

--- client/src/main/java/pl/edu/pjwstk/client/controller/ClientSideController.java
+++ client/src/main/java/pl/edu/pjwstk/client/controller/ClientSideController.java
@@ -2,6 +2,7 @@ package pl.edu.pjwstk.client.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -9,12 +10,17 @@ import org.springframework.web.client.RestTemplate;
 @RestController
 public class ClientSideController {
 
-    private final String trainsList = "http://127.0.0.1:9000/trains";
-    private final String trainDetail = "http://simulator/9000/trains/";
+    private final String trainsList = "http://simulator:9000/trains";
 
     @GetMapping("/trains")
     public ResponseEntity getTrains() {
         RestTemplate template = new RestTemplate();
         return template.getForEntity(trainsList, String.class);
+    }
+
+    @GetMapping("/trains/{id}")
+    public ResponseEntity getTrains(@PathVariable int id) {
+        RestTemplate template = new RestTemplate();
+        return template.getForEntity(trainsList+'/'+id, String.class);
     }
 }
