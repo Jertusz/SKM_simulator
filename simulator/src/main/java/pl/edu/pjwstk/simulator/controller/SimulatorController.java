79bc5ebff0adccd8fc551cb549a5215aa4e6ff90
Jedrzej Szadejko <jendziu@gmail.com>
2020-12-14 12:17:04 +0100

    Modified client to work with new backend

--- simulator/src/main/java/pl/edu/pjwstk/simulator/controller/SimulatorController.java
+++ simulator/src/main/java/pl/edu/pjwstk/simulator/controller/SimulatorController.java
@@ -4,10 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import pl.edu.pjwstk.simulator.models.Train;
 import pl.edu.pjwstk.simulator.service.SimulatorService;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/move")
